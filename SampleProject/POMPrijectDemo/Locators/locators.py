d21d637c9501423338bd747b953f6becd015edf5
Vishnusankarreddy <vishnusankar.reddy@softsuave.com>
2024-04-25 22:48:45 +0530

    POModel

---
+++ SampleProject/POMPrijectDemo/Locators/locators.py
@@ -0,0 +1,10 @@
+class Locators():
+
+    # login page objects
+    username_textbox_Name = "username"
+    password_textbox_Name = "Password"
+    login_button_id = "Login"
+
+    # home page objects
+    Zenon_e_Pesado_link_linkText = "Zenon e Pesado"
+    logout_link_linkText = "Logout"
---
+++ SampleProject/POMPrijectDemo/Pages/homePage.py
@@ -0,0 +1,7 @@
+class HomePage():
+
+    def __init__(self, driver):
+        self.driver = driver
+
+        self.Zenon_e_Pesado_link_linkText = "Zenon e Pesado"
+        self.logout_link_linkText = "Logout"
